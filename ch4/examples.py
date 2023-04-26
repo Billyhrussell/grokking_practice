51a865c367267f5674514ab26f3da1e028b1dce8
Billy Russell <billy.hrussell@gmail.com>
2023-04-26 12:05:48 -0700

    fin ch4 ex

--- ch4/examples.py
+++ ch4/examples.py
@@ -25,9 +25,55 @@ def look_for_key(main_box):
 def look_for_key_2(main_box):
     for item in box:
         if item.is_a_box:
-            # RECURSION! 
+            # RECURSION!
             look_for_key(item)
         elif item.is_a_key:
             print "found the key!"
 
 
+# BASE CASE AND RECURSIVE CASE
+def countdown(i):
+    print i
+    # BASE CASE
+    if i <= 1:
+        return
+    # RECURSIVE CASE
+    else:
+        countdown(i-1)
+
+# THE CALL STACK
+def greet(name):
+    print "hello, " + name + "!"
+    greet2(name)
+    print "getting ready to say bye..."
+    bye()
+
+def greet2(name):
+    print "how are you, " + name + "?"
+
+def bye():
+    print "okay bye!"
+
+# THE FLOW
+# STACK : greet(name)
+# STACK: greet2(name)-> greet(name)
+# STACK: greet(name)
+# STACK: bye() -> greet(name)
+# STACK: greet(name)
+# STACK: empty
+
+
+# factorial recursion function
+def fact(x):
+    if x == 1:
+        return 1
+    else
+        return x + fact(x-1)
+
+# THE FLOW:
+# STACK: fact(3)
+# STACK: fact(2) -> fact(3)
+# STACK: fact(1) -> fact(2) -> fact(3)
+# STACK: fact(2) returns 2 + 1 -> fact(3)
+# STACK: fact(3) returns 3 + 3
+# STACK: empty 
